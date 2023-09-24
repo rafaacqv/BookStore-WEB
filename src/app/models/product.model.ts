fa8a8183a85d6cc4a3a1d10d172d31ff14d00461
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-24 00:10:45 -0300

    Add product and pagination models

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  author: string;
+  isbn: string;
+  category: string;
+  format: string;
+}
