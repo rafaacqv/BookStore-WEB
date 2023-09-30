0f692bb5e989ca68e9e2b85a8ee936f3c608aa47
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-30 00:30:05 -0300

    Add internal server error component

--- src/app/core/server-error/server-error.component.ts
+++ src/app/core/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-error.component.css']
 })
 export class ServerErrorComponent {
+  constructor(private router: Router) {}
 
+  navigateHome() {
+    this.router.navigateByUrl('/');
+  }
 }
