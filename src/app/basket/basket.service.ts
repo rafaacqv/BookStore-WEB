6c5172232ae01a46cf6376be62d517b264e58bf0
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-01 00:17:06 -0300

    wip basket service

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Basket } from '../shared/models/basket.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<Basket | null>(null);
+  basketSource$ = this.basketSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string) {
+    return this.http.get<Basket>(this.baseUrl + 'basket?id=' + id).subscribe({
+      next: basket => this.basketSource.next(basket)
+    });
+  }
+
+  setBasket(basket: Basket) {
+    return this.http.post<Basket>(this.baseUrl + 'basket', basket).subscribe({
+      next: basket => this.basketSource.next(basket)
+    });
+  }
+
+  getCurrentBasketValue() {
+    return this.basketSource.value;
+  }
+
+
 }
