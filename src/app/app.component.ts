aaf7e6f7d07b8a4756899d6160831dc2dbb07aee
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-02 21:07:23 -0300

    Persist basket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './core/material/loading.service';
 import { Subject } from 'rxjs';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,11 @@ import { Subject } from 'rxjs';
 export class AppComponent implements OnInit {
   isLoading: Subject<boolean> = this.loadingService.isLoading;
 
-  constructor(private loadingService: LoadingService) {}
+  constructor(private loadingService: LoadingService,
+              private basketService: BasketService) {}
 
   ngOnInit(): void {
-
+    const basket_id = localStorage.getItem('basket_id');
+    if(basket_id) this.basketService.getBasket(basket_id);
   }
 }
