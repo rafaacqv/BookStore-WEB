a4078c517ab3ae821e59c4dcb16a7591866872b8
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-02 21:03:19 -0300

    Fix bug in basket service

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { Basket, BasketItem } from '../shared/models/basket.model';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../shared/models/product.model';
--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/product.model';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,10 +14,15 @@ export class ProductDetailsComponent implements OnInit{
   numbers: Array<number> = [1, 2, 3, 4, 5];
 
   constructor(private shopService: ShopService,
-              private activatedRoute: ActivatedRoute) {}
+              private activatedRoute: ActivatedRoute,
+              private basketService: BasketService) {}
 
   ngOnInit(): void {
     this.getProduct();
+  }
+
+  addItemToBasket() {
+    this.product && this.basketService.addItemToBasket(this.product);
   }
 
   getProduct() {
