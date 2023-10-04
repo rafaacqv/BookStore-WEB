1ffe5cb9c8b4f9e48a5b235697cf4748b772f4ab
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-03 22:25:45 -0300

    Create shopping empty interface

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BasketService } from './basket.service';
 import { Basket, BasketItem } from '../shared/models/basket.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-basket',
@@ -8,9 +9,14 @@ import { Basket, BasketItem } from '../shared/models/basket.model';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent {
-  constructor(public basketService: BasketService) {}
+  constructor(public basketService: BasketService,
+              private router: Router) {}
 
   numbers: Array<number> = [1, 2, 3, 4, 5];
+
+  navigateHome() {
+    this.router.navigateByUrl('/');
+  }
 
   updateQuantity(basket: Basket, item: BasketItem) {
     var result = basket.items.map(e => e.id == item.id ? {...e, quantity: item.quantity}: e);
