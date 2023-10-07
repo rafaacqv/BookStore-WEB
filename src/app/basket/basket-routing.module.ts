55706f9b71c0c70e986db7b00eca2944cd6d901d
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-07 16:55:15 -0300

    Start account component

--- src/app/basket/basket-routing.module.ts
+++ src/app/basket/basket-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BasketComponent } from './basket.component';
 
@@ -10,7 +9,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forChild(routes)
   ],
   exports: [
