763f7a55da4d554d7860d9971f3ab5a67c20cb27
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-08 00:20:09 -0300

    Implemented login feature

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { CustomErrorStateMatcher } from 'src/app/core/form-validators/CustomErrorStateMatcher ';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,19 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   });
+
+  matcher = new CustomErrorStateMatcher();
 
   constructor(private accountService: AccountService, private router: Router){}
 
   onSubmit() {
-    this.accountService.login(this.loginForm.value).subscribe({
-      next: () => this.router.navigateByUrl('/')
-    })
+    if(!this.loginForm.invalid) {
+      this.accountService.login(this.loginForm.value).subscribe({
+        next: () => this.router.navigateByUrl('/')
+      })
+    }
   }
 }
