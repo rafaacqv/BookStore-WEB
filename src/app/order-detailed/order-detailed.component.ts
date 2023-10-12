da74649c056e52249ada2cc1a6a701e58d0f0170
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-12 17:24:38 -0300

    Create order details component

--- src/app/order-detailed/order-detailed.component.ts
+++ src/app/order-detailed/order-detailed.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../shared/models/order.model';
+import { ActivatedRoute } from '@angular/router';
+import { OrdersService } from '../orders/orders.service';
 
 @Component({
   selector: 'app-order-detailed',
   templateUrl: './order-detailed.component.html',
   styleUrls: ['./order-detailed.component.css']
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  order?: Order;
+
+  constructor(private orderService: OrdersService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.getOrder();
+  }
+
+  getOrder() {
+    const id = this.route.snapshot.paramMap.get('id');
+    id && this.orderService.getOrderDetailed(+id).subscribe({
+      next: order => {
+      this.order = order;
+    }
+  })
+}
 
 }
